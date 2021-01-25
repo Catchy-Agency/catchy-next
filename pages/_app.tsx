d4db711b471147d24cbe9fd24eafdcc1dfb509fd
Alex Burner <alex.burner@gmail.com>
2021-01-24 20:32:50 -0800

    Add key listener for preview mode

---
+++ pages/_app.tsx
@@ -0,0 +1,32 @@
+import { AppComponent } from 'next/dist/next-server/lib/router/router'
+
+// TODO extract to component
+if (typeof window !== 'undefined') {
+  let index = 0
+  const keyword = 'preview'
+  const end = keyword.length - 1
+  document.addEventListener('keydown', (e) => {
+    if (index === end) {
+      // Win condition
+      index = 0
+      const origin = window.location.origin
+      const path = window.location.pathname
+      const dest = `${origin}/api/preview?path=${path}`
+      window.location.replace(dest)
+    } else if (keyword[index] === e.key) {
+      // Step forward
+      index++
+    } else {
+      // Lose
+      index = 0
+    }
+  })
+}
+
+const App: AppComponent = ({ Component, pageProps }) => (
+  <>
+    <Component {...pageProps}></Component>
+  </>
+)
+
+export default App
