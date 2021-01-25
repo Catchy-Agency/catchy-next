65dc68309c1323239e1f7b20fcebd45ee1a3be0c
Alex Burner <alex.burner@gmail.com>
2021-01-24 19:44:03 -0800

    Update preview: s/slug/path/

--- pages/api/exit-preview.ts
+++ pages/api/exit-preview.ts
@@ -1,7 +1,7 @@
 import { NextApiHandler } from 'next'
 
 const exitPreview: NextApiHandler = (req, res) => {
-  let dest = typeof req.query.slug === 'string' ? req.query.slug : '/'
+  let dest = typeof req.query.path === 'string' ? req.query.path : '/'
   if (dest[0] !== '/') dest = '/' + dest
   res.clearPreviewData()
   res.writeHead(307, { Location: dest })
--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -1,7 +1,7 @@
 import { NextApiHandler } from 'next'
 
 const preview: NextApiHandler = (req, res) => {
-  let dest = typeof req.query.slug === 'string' ? req.query.slug : '/'
+  let dest = typeof req.query.path === 'string' ? req.query.path : '/'
   if (dest[0] !== '/') dest = '/' + dest
   res.setPreviewData({})
   res.writeHead(307, { Location: dest })
