9e3827256be8f174366c650517f518b8515622ac
Alex Burner <alex.burner@gmail.com>
2021-02-07 08:20:48 -0800

    Add CardContentLink, begin Medium

--- components/blocks/ContentLinkSet.tsx
+++ components/blocks/ContentLinkSet.tsx
@@ -3,8 +3,6 @@ import { FC } from 'react'
 
 import { PrimaryPageBySlug_primaryPage_blocks_ContentLinkSetRecord } from '../../gql/types/PrimaryPageBySlug'
 
-// Todo: display size
-
 const urlPrefixes = {
   ContentPageRecord: '/pages/',
   ContentPostRecord: '/posts/',
@@ -16,9 +14,9 @@ export const ContentLinkSet: FC<{
 }> = ({ block }) => {
   switch (block.displaySize) {
     case 'Card':
-      return <LargeContentLinkSet block={block} />
+      return <CardContentLinkSet block={block} />
     case 'Medium':
-      return <LargeContentLinkSet block={block} />
+      return <MediumContentLinkSet block={block} />
     case 'Large':
       return <LargeContentLinkSet block={block} />
     default:
@@ -26,7 +24,102 @@ export const ContentLinkSet: FC<{
   }
 }
 
-export const LargeContentLinkSet: FC<{
+const CardContentLinkSet: FC<{
+  block: PrimaryPageBySlug_primaryPage_blocks_ContentLinkSetRecord
+}> = ({ block }) => {
+  const backgroundColor = block.backgroundColor?.hex || undefined
+  const color = block.textColor?.hex || undefined
+  const cta = block.callToActionLabel
+  return (
+    <section
+      className="section"
+      key={block.id}
+      style={{ backgroundColor, color }}
+    >
+      <div className="container">
+        <div className="columns">
+          {block.links.map((link) => {
+            const title = link.seo?.title || undefined
+            const description = link.seo?.description || undefined
+            const image = link.seo?.image?.url || undefined
+            const url = urlPrefixes[link.__typename] + link.slug
+            return (
+              <div key={link.id} className="column">
+                <div className="card">
+                  <div className="card-image">
+                    <figure className="image">
+                      <img src={image} title={title} alt={title} />
+                    </figure>
+                  </div>
+                  <div className="card-content">
+                    {title && (
+                      <div className="title is-6" style={{ color }}>
+                        {title}
+                      </div>
+                    )}
+                    {description && (
+                      <div className="content">{description}</div>
+                    )}
+                    <a href={url} title={title}>
+                      {cta}
+                    </a>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+const MediumContentLinkSet: FC<{
+  block: PrimaryPageBySlug_primaryPage_blocks_ContentLinkSetRecord
+}> = ({ block }) => {
+  const backgroundColor = block.backgroundColor?.hex || undefined
+  const color = block.textColor?.hex || undefined
+  const cta = block.callToActionLabel
+  const alternating = block.alternatingAlignments
+  return (
+    <section
+      className="section"
+      key={block.id}
+      style={{ backgroundColor, color }}
+    >
+      <div className={classNames('container', { alternating })}>
+        {block.links.map((link) => {
+          const title = link.seo?.title || undefined
+          const description = link.seo?.description || undefined
+          const image = link.seo?.image?.url || undefined
+          const url = urlPrefixes[link.__typename] + link.slug
+          return (
+            <div key={link.id} className="columns">
+              <div className="column is-7">
+                {title && (
+                  <div className="title is-2" style={{ color }}>
+                    {title}
+                  </div>
+                )}
+                {description && <div className="content">{description}</div>}
+                <a className="button" href={url} title={title}>
+                  {cta}
+                </a>
+              </div>
+              <div className="column is-5">
+                <figure className="image">
+                  <img src={image} title={title} alt={title} />
+                </figure>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </section>
+  )
+}
+
+const LargeContentLinkSet: FC<{
   block: PrimaryPageBySlug_primaryPage_blocks_ContentLinkSetRecord
 }> = ({ block }) => {
   const backgroundColor = block.backgroundColor?.hex || undefined
