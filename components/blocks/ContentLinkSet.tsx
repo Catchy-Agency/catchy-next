afc8a5720de6196d822f6b6e91579028a5b04008
Alex Burner <alex.burner@gmail.com>
2021-02-06 18:48:06 -0800

    Add alternation to content links

--- components/blocks/ContentLinkSet.tsx
+++ components/blocks/ContentLinkSet.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import { FC } from 'react'
 
 import { PrimaryPageBySlug_primaryPage_blocks_ContentLinkSetRecord } from '../../gql/types/PrimaryPageBySlug'
@@ -13,19 +14,32 @@ const urlPrefixes = {
 export const ContentLinkSet: FC<{
   block: PrimaryPageBySlug_primaryPage_blocks_ContentLinkSetRecord
 }> = ({ block }) => {
-  console.log(block)
+  switch (block.displaySize) {
+    case 'Card':
+      return <LargeContentLinkSet block={block} />
+    case 'Medium':
+      return <LargeContentLinkSet block={block} />
+    case 'Large':
+      return <LargeContentLinkSet block={block} />
+    default:
+      return null
+  }
+}
+
+export const LargeContentLinkSet: FC<{
+  block: PrimaryPageBySlug_primaryPage_blocks_ContentLinkSetRecord
+}> = ({ block }) => {
   const backgroundColor = block.backgroundColor?.hex || undefined
   const color = block.textColor?.hex || undefined
-  // const alternating = block.alternatingAlignments
-  // const display = block.displaySize
   const cta = block.callToActionLabel
+  const alternating = block.alternatingAlignments
   return (
     <section
       className="section"
       key={block.id}
       style={{ backgroundColor, color }}
     >
-      <div className="container">
+      <div className={classNames('container', { alternating })}>
         {block.links.map((link) => {
           const title = link.seo?.title || undefined
           const description = link.seo?.description || undefined
