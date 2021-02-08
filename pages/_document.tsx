ff692a923d4c27d0fe62674c4e2aec901304c189
Alex Burner <alex.burner@gmail.com>
2021-02-08 15:05:26 -0800

    Add html lang for lighthouse

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Head, Html, Main, NextScript } from 'next/document'
 export default class extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head></Head>
         <body className="has-navbar-fixed-top">
           <Main />
