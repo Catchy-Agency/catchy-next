1b4634ccbc7b1837712f156a1cb974bbf464e437
Alex Burner <alex.burner@gmail.com>
2021-02-09 20:08:50 -0800

    Add /blog categories

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -4,16 +4,16 @@ import Head from 'next/head'
 
 import { createSubscription, Subscription } from '../../gql/dato-cms'
 import { allContentPosts } from '../../gql/queries/content-posts'
-import { PrimaryPageBySlug } from '../../gql/types/PrimaryPageBySlug'
+import { AllContentPosts } from '../../gql/types/AllContentPosts'
 import { PreviewBanner } from '../../components/cms/PreviewBanner'
 import { PageError } from '../../components/cms/PageError'
 import { Header } from '../../components/Header'
 import { Footer } from '../../components/Footer'
 
 const ContentPosts: NextPage<{
-  subscription: Subscription<PrimaryPageBySlug>
+  subscription: Subscription<AllContentPosts>
 }> = ({ subscription }) => {
-  const { data, error, status } = useQuerySubscription<PrimaryPageBySlug>(
+  const { data, error, status } = useQuerySubscription<AllContentPosts>(
     subscription,
   )
 
@@ -28,9 +28,24 @@ const ContentPosts: NextPage<{
       <PreviewBanner status={status} />
       {error && <PageError error={error} />}
       {data?.header && <Header header={data?.header} />}
-
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-2">
+              <h5 className="title is-5">Categories</h5>
+              <hr />
+              {data?.allCategories.map((cat) => (
+                <div key={cat.id} className="mb-2">
+                  <a href={`/blog/category/${cat.slug}`}>
+                    <span className="tag is-medium">{cat.name}</span>
+                  </a>
+                </div>
+              ))}
+            </div>
+            <div className="column is-10"></div>
+          </div>
+        </div>
+      </section>
       {data?.footer && <Footer footer={data?.footer} />}
     </>
   )
@@ -38,7 +53,7 @@ const ContentPosts: NextPage<{
 
 export const getStaticProps: GetStaticProps = async (context) => ({
   props: {
-    subscription: await createSubscription<PrimaryPageBySlug>(
+    subscription: await createSubscription<AllContentPosts>(
       context,
       allContentPosts,
     ),
