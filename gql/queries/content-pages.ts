d0b27cc7a54097aacdea541bd12f0ad5d4b699f4
Alex Burner <alex.burner@gmail.com>
2021-02-11 14:27:42 -0800

    Add content page by slug

--- gql/queries/content-pages.ts
+++ gql/queries/content-pages.ts
@@ -7,3 +7,307 @@ export const allContentPageSlugs = gql`
     }
   }
 `
+
+export const contentPageBySlug = gql`
+  query ContentPageBySlug($slug: String) {
+    contentPage(filter: { slug: { eq: $slug } }) {
+      title
+      parent {
+        title
+        slug
+        parent {
+          title
+          slug
+          parent {
+            title
+            slug
+            parent {
+              title
+              slug
+              parent {
+                title
+                slug
+              }
+            }
+          }
+        }
+      }
+      children {
+        title
+        slug
+        seo {
+          title
+          description
+          image {
+            responsiveImage(
+              imgixParams: { fit: crop, ar: "16:9", w: 750, auto: format }
+            ) {
+              aspectRatio
+              height
+              width
+              sizes
+              src
+              srcSet
+              alt
+              title
+              base64
+            }
+          }
+        }
+      }
+      blocks {
+        __typename
+        ... on ButtonExternalRecord {
+          id
+          label
+          url
+          openInANewTab
+        }
+        ... on ButtonInternalRecord {
+          id
+          label
+          link {
+            __typename
+            ... on ContentPageRecord {
+              slug
+            }
+            ... on ContentPostRecord {
+              slug
+            }
+            ... on PrimaryPageRecord {
+              slug
+            }
+          }
+        }
+        ... on ColumnRowRecord {
+          id
+          columns {
+            id
+            title
+            image {
+              responsiveImage(
+                imgixParams: { fit: clip, maxW: 1344, auto: format }
+              ) {
+                aspectRatio
+                height
+                width
+                sizes
+                src
+                srcSet
+                alt
+                title
+                base64
+              }
+            }
+            text
+            blocks {
+              __typename
+              ... on ButtonExternalRecord {
+                id
+                label
+                url
+                openInANewTab
+              }
+              ... on ButtonInternalRecord {
+                id
+                label
+                link {
+                  __typename
+                  ... on ContentPageRecord {
+                    slug
+                  }
+                  ... on ContentPostRecord {
+                    slug
+                  }
+                  ... on PrimaryPageRecord {
+                    slug
+                  }
+                }
+              }
+              ... on RichTextRecord {
+                id
+                richText
+              }
+              ... on VideoRecord {
+                id
+                video {
+                  height
+                  provider
+                  providerUid
+                  thumbnailUrl
+                  title
+                  url
+                  width
+                }
+              }
+            }
+            sizeModifier
+          }
+          verticallyCenterAcrossColumns
+        }
+        ... on ContentLinkSetRecord {
+          id
+          displaySize
+          callToActionLabel
+          alternatingAlignments
+          backgroundColor {
+            hex
+          }
+          textColor {
+            hex
+          }
+          links {
+            __typename
+            ... on PrimaryPageRecord {
+              id
+              slug
+              seo {
+                title
+                description
+                image {
+                  responsiveImage(
+                    imgixParams: { fit: crop, ar: "16:9", w: 750, auto: format }
+                  ) {
+                    aspectRatio
+                    height
+                    width
+                    sizes
+                    src
+                    srcSet
+                    alt
+                    title
+                    base64
+                  }
+                }
+              }
+            }
+            ... on ContentPostRecord {
+              id
+              slug
+              seo {
+                title
+                description
+                image {
+                  responsiveImage(
+                    imgixParams: { fit: crop, ar: "16:9", w: 750, auto: format }
+                  ) {
+                    aspectRatio
+                    height
+                    width
+                    sizes
+                    src
+                    srcSet
+                    alt
+                    title
+                    base64
+                  }
+                }
+              }
+            }
+            ... on ContentPageRecord {
+              id
+              slug
+              seo {
+                title
+                description
+                image {
+                  responsiveImage(
+                    imgixParams: { fit: crop, ar: "16:9", w: 750, auto: format }
+                  ) {
+                    aspectRatio
+                    height
+                    width
+                    sizes
+                    src
+                    srcSet
+                    alt
+                    title
+                    base64
+                  }
+                }
+              }
+            }
+          }
+        }
+        ... on ImageSetRecord {
+          id
+          imageSet {
+            id
+            responsiveImage(
+              imgixParams: { fit: clip, maxW: 1344, auto: format }
+            ) {
+              aspectRatio
+              height
+              width
+              sizes
+              src
+              srcSet
+              alt
+              title
+              base64
+            }
+          }
+          verticallyCenterAcrossImages
+        }
+        ... on RichTextRecord {
+          id
+          richText
+        }
+        ... on TitleRecord {
+          id
+          title
+        }
+        ... on VideoRecord {
+          id
+          video {
+            height
+            provider
+            providerUid
+            thumbnailUrl
+            title
+            url
+            width
+          }
+        }
+      }
+      slug
+      _seoMetaTags {
+        attributes
+        content
+        tag
+      }
+    }
+    site: _site {
+      faviconMetaTags {
+        attributes
+        content
+        tag
+      }
+    }
+    header {
+      logo {
+        url
+        title
+        alt
+      }
+      links {
+        id
+        title
+        slug
+      }
+      contactLinkLabel
+    }
+    footer {
+      title
+      nameLabel
+      emailAddressLabel
+      messageLabel
+      contactText
+      socialLinks {
+        id
+        fontAwesomeIcon
+        url
+      }
+    }
+  }
+`
--- pages/pages/[slug].tsx
+++ pages/pages/[slug].tsx
@@ -1,5 +1,82 @@
-import { NextPage } from 'next'
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
+import { useQuerySubscription, renderMetaTags } from 'react-datocms'
+import Head from 'next/head'
+import Link from 'next/link'
 
-const ContentPage: NextPage = () => <>ContentPage</>
+import {
+  createSubscription,
+  getContentPagePaths,
+  Subscription,
+} from '../../gql/dato-cms'
+import { contentPageBySlug } from '../../gql/queries/content-pages'
+import { ContentPageBySlug } from '../../gql/types/ContentPageBySlug'
+import { PreviewBanner } from '../../components/cms/PreviewBanner'
+import { PageError } from '../../components/cms/PageError'
+import { Header } from '../../components/Header'
+import { Footer } from '../../components/Footer'
+import { BlockSections } from '../../components/BlockSections'
+
+// const extractAncestors = (page: Conte)
+
+const ContentPage: NextPage<{
+  subscription: Subscription<ContentPageBySlug>
+}> = ({ subscription }) => {
+  const { data, error, status } = useQuerySubscription<ContentPageBySlug>(
+    subscription,
+  )
+
+  // const ancestors = extractAncestors(data?.contentPage, []);
+
+  return (
+    <>
+      <Head>
+        {renderMetaTags([
+          ...(data?.contentPage?._seoMetaTags || []),
+          ...(data?.site.faviconMetaTags || []),
+        ])}
+      </Head>
+      <PreviewBanner status={status} />
+      {error && <PageError error={error} />}
+      {data?.header && <Header header={data?.header} />}
+      <header className="section">
+        <div className="container">
+          <nav className="breadcrumb" aria-label="breadcrumbs">
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/blog">Blog</a>
+              </li>
+              <li className="is-active">
+                <a href="#" aria-current="page">
+                  {data?.contentPage?.title}
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <h1 className="title is-1">{data?.contentPage?.title}</h1>
+        </div>
+      </header>
+      <BlockSections blocks={data?.contentPage?.blocks || []} />
+      {data?.footer && <Footer footer={data?.footer} />}
+    </>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => ({
+  paths: await getContentPagePaths(),
+  fallback: false,
+})
+
+export const getStaticProps: GetStaticProps = async (context) => ({
+  props: {
+    subscription: await createSubscription<ContentPageBySlug>(
+      context,
+      contentPageBySlug,
+      { slug: context?.params?.slug },
+    ),
+  },
+})
 
 export default ContentPage
