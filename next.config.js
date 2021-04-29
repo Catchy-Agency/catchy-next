b02d8e137887a0a171dcc126b20b097a6100398e
Alex Burner <alex.burner@gmail.com>
2021-04-29 09:09:01 -0700

    Add referrer-policy header

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const { createSecureHeaders } = require('next-secure-headers')
 const redirects = require('./redirects/redirects')
 
 module.exports = {
-  headers: async () => [{ source: '/(.*)', headers: createSecureHeaders() }],
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: createSecureHeaders({ referrerPolicy: 'same-origin' }),
+    },
+  ],
   redirects: async () => redirects,
 }
