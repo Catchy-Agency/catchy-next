33d200f3e2a496e922d3c45b54077f3e9af62b33
jganstf <jesse@temperandforge.com>
2022-09-29 07:42:57 -0700

    open sans font inclusion

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,7 @@ export default class extends Document {
             crossOrigin={'true'}
           /> */}
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Open+Sans:wght@400;700&display=swap"
             rel="stylesheet"
           />
           {/* Font Awesome */}
