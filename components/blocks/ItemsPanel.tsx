cc645515131ce1ff7f26b1236a0bf22d83389f26
Alex Burner <alex.burner@gmail.com>
2023-09-16 13:28:14 -0700

    ItemsPanel: add mobile version

--- components/blocks/ItemsPanel.tsx
+++ components/blocks/ItemsPanel.tsx
@@ -38,53 +38,75 @@ export const ItemsPanel: FC<{
           {block.panelItems.map((item, i) => {
             const isActiveService = activeIndex === i
             return (
-              <div
-                key={item.id}
-                className="is-flex is-align-items-center is-clickable"
-                onClick={() => setActiveIndex(i)}
-              >
+              <div key={item.id}>
                 <div
-                  style={{
-                    background:
-                      'radial-gradient(101.36% 145.94% at 0% 0%, rgba(29, 188, 131, 0.43) 4.96%, rgba(0, 0, 0, 0) 100%) #0d202c',
-                    minWidth: ICON_SIZE,
-                    width: ICON_SIZE,
-                    height: ICON_SIZE,
-                    borderRadius: 1000,
-                    position: 'relative',
-                    zIndex: 10,
-                  }}
-                >
-                  {item.listImage?.responsiveImage && (
-                    <img
-                      src={item.listImage.responsiveImage.src}
-                      style={{
-                        width: ICON_SIZE,
-                        height: ICON_SIZE,
-                        objectFit: 'cover',
-                      }}
-                    />
-                  )}
-                </div>
-                <div
-                  className="is-flex-grow-1 is-flex is-align-items-center"
-                  style={{
-                    background: isActiveService
-                      ? 'radial-gradient(101.36% 145.94% at 0% 0%, rgba(29, 188, 131, 0.43) 4.96%, rgba(0, 0, 0, 0) 100%)'
-                      : undefined,
-                    height: ICON_SIZE,
-                    marginLeft: `calc(-${ICON_SIZE} / 2)`,
-                    paddingLeft: `${ICON_SIZE}`,
-                  }}
+                  className="is-flex is-align-items-center is-clickable"
+                  onClick={() => setActiveIndex(i)}
                 >
                   <div
-                    className={classNames('IP-list-text', {
-                      'is-active-service': isActiveService,
-                    })}
+                    style={{
+                      background:
+                        'radial-gradient(101.36% 145.94% at 0% 0%, rgba(29, 188, 131, 0.43) 4.96%, rgba(0, 0, 0, 0) 100%) #0d202c',
+                      minWidth: ICON_SIZE,
+                      width: ICON_SIZE,
+                      height: ICON_SIZE,
+                      borderRadius: 1000,
+                      position: 'relative',
+                      zIndex: 10,
+                    }}
                   >
-                    {item.listTitle}
+                    {item.listImage?.responsiveImage && (
+                      <img
+                        src={item.listImage.responsiveImage.src}
+                        style={{
+                          width: ICON_SIZE,
+                          height: ICON_SIZE,
+                          objectFit: 'cover',
+                        }}
+                      />
+                    )}
+                  </div>
+                  <div
+                    className="is-flex-grow-1 is-flex is-align-items-center"
+                    style={{
+                      background: isActiveService
+                        ? 'radial-gradient(101.36% 145.94% at 0% 0%, rgba(29, 188, 131, 0.43) 4.96%, rgba(0, 0, 0, 0) 100%)'
+                        : undefined,
+                      height: ICON_SIZE,
+                      marginLeft: `calc(-${ICON_SIZE} / 2)`,
+                      paddingLeft: `${ICON_SIZE}`,
+                    }}
+                  >
+                    <div
+                      className={classNames('IP-list-text', {
+                        'is-active-service': isActiveService,
+                      })}
+                    >
+                      {item.listTitle}
+                    </div>
                   </div>
                 </div>
+                {isActiveService && (
+                  <div
+                    className="pt-4 is-hidden-tablet"
+                    style={{
+                      paddingLeft: `calc(${ICON_SIZE} * 1.5)`,
+                    }}
+                  >
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: activeItem.detailText ?? '',
+                      }}
+                    />
+                    {activeItem.detailLink?.slug && (
+                      <Link href={`/${activeItem.detailLink.slug}`}>
+                        <a className="button is-primary mt-4">
+                          {activeItem.detailLinkLabel || 'Learn More'}
+                        </a>
+                      </Link>
+                    )}
+                  </div>
+                )}
               </div>
             )
           })}
@@ -92,7 +114,7 @@ export const ItemsPanel: FC<{
       </div>
       {/* Detail */}
       <div
-        className="column"
+        className="column is-hidden-mobile"
         style={{ backgroundColor: '#143144' }}
         key={activeItem.id}
       >
