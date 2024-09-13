e67761583d628511ddc03c9a31fcca38c2d3dcc2
Francisco Crema <francisco.crema@miracledevs.com>
2024-09-13 16:31:48 -0300

    CWU-26 -> update footer WIP

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,71 +1,69 @@
-import classNames from 'classnames'
-import Link from 'next/link'
-import { FC } from 'react'
+import classNames from 'classnames';
+import { FC } from 'react';
 
-import { PrimaryPageBySlug_footer } from '../gql/types/PrimaryPageBySlug'
-import { pageview } from '../util/gtag'
+import Link from 'next/link';
+import { PrimaryPageBySlug_footer } from '../gql/types/PrimaryPageBySlug';
+import { pageview } from '../util/gtag';
+import { IconLogo } from './icons';
 
 export const Footer: FC<{
-  footer: PrimaryPageBySlug_footer
+  footer: PrimaryPageBySlug_footer;
 }> = ({ footer }) => (
   <footer className="section _has-background-grey-darker">
     <div className="container is-max-widescreen">
       <div className="columns">
-        <div className="column">
-          <div className="is-flex is-align-items-center">
-            {footer.contactEmail && (
-              <a
-                href={`mailto:${footer.contactEmail}`}
-                rel="noreferrer"
-                className="mr-5"
-              >
-                {footer.contactEmail}
-              </a>
-            )}
-            {footer.socialLinks?.map(
-              (link) =>
-                link && (
-                  <a
-                    key={link.fontAwesomeIcon}
-                    href={link.url || undefined}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="is-size-3"
-                  >
-                    <span className="fa-stack">
-                      <i
-                        className={classNames(
-                          link.fontAwesomeIcon,
-                          'fa-stack-1x',
-                          'has-text-turquoise',
-                        )}
-                      />
-                    </span>
-                  </a>
-                ),
-            )}
-          </div>
-          <div className="mt-6">
-            {footer.contactText && (
-              <div
-                className="content"
-                dangerouslySetInnerHTML={{
-                  __html: footer.contactText || '',
-                }}
-              />
-            )}
-          </div>
-          {footer.directionsUrl && (
-            <div className="mt-5">
-              <a href={footer.directionsUrl} rel="noreferrer">
-                directions
-              </a>
-            </div>
+        <div className="column is-flex-direction-column">
+          {footer.socialLinks?.map(
+            (link) =>
+              link && (
+                <a
+                  key={link.fontAwesomeIcon}
+                  href={link.url || undefined}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="is-size-3"
+                >
+                  <span className="fa-stack">
+                    <i
+                      className={classNames(
+                        link.fontAwesomeIcon,
+                        'fa-stack-1x',
+                        'has-text-turquoise',
+                        'color-hover',
+                      )}
+                    />
+                  </span>
+                </a>
+              ),
           )}
+          <div className="control mt-4">
+            <button
+              className="button is-primary"
+              onClick={() => pageview('/contact')}
+              type="submit"
+            >
+              Join Our Mailing List
+            </button>
+          </div>
+          <div className="footer-bottom">
+            <div className="is-flex-direction-column">
+              <Link href="/">
+                <a className="navbar-item is-justify-content-flex-start">
+                  {IconLogo}
+                </a>
+              </Link>
+              {footer.richText && (
+                <div
+                  className="mt-6 content privacy"
+                  dangerouslySetInnerHTML={{ __html: footer.richText }}
+                />
+              )}
+            </div>
+          </div>
         </div>
         <div className="column mt-3">
           {footer.formTitle && (
-            <h4 className="has-text-primary mb-6">{footer.formTitle}</h4>
+            <h4 className="has-text-white mb-6">{footer.formTitle}</h4>
           )}
           <form
             id="contact-form"
@@ -124,23 +122,6 @@ export const Footer: FC<{
           </form>
         </div>
       </div>
-      <div className="footer-bottom">
-        <Link href="/">
-          <a className="navbar-item">
-            <img
-              src={footer.footerLogo?.url}
-              alt={footer.footerLogo?.alt || undefined}
-              title={footer.footerLogo?.title || undefined}
-            />
-          </a>
-        </Link>
-        {footer.richText && (
-          <div
-            className="mb-0 content"
-            dangerouslySetInnerHTML={{ __html: footer.richText }}
-          />
-        )}
-      </div>
     </div>
   </footer>
-)
+);
