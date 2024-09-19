00148d24ef74cc5f4a1ddfac7acd6d17904e7419
Alex Burner <alex.burner@gmail.com>
2024-09-18 20:25:58 -0700

    Re-enable google tracking with new ID
    Originally disabled in: https://github.com/Catchy-Agency/catchy-next/commit/4602c21183dd3febf7e09f61dc18c2d69ce7aaa1
    
    And the other one added in: https://github.com/Catchy-Agency/catchy-next/commit/d04055a1d0b07de3141ed87d0b9f88935a2566d5
    
    And I couldn't tell you the story behind either of those
    
    But now we wanna revive the first style, but with a new ID

--- util/gtag.ts
+++ util/gtag.ts
@@ -1,7 +1,7 @@
-export const GA_TRACKING_ID = 'UA-75221843-1'
+export const GA_TRACKING_ID = 'G-43K6KT8E8B'
 
-export const pageview = (_path: string): void => {
+export const pageview = (path: string): void => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
-  // const gtag: (...args: any[]) => void = (window as any).gtag
-  // gtag && gtag('config', GA_TRACKING_ID, { page_path: path })
+  const gtag: (...args: any[]) => void = (window as any).gtag
+  gtag && gtag('config', GA_TRACKING_ID, { page_path: path })
 }
