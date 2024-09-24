c38ece1ecbeed5b46b2ed6405212bdfd68eb9f23
Guido Viani <guido.viani@miracledevs.com>
2024-09-24 14:52:37 -0300

    CWU-53 -> WIP dropdown mobile

--- components/content-links/cards/PanelCards.tsx
+++ components/content-links/cards/PanelCards.tsx
@@ -1,23 +1,76 @@
-import { FC } from 'react';
+import Link from 'next/link';
+import { FC, useState } from 'react';
+import { useMediaQuery } from '../../hooks/useMediaQuery';
+import { AngleDown } from '../../icons';
 
 export const PanelCards: FC<{
   width?: number;
   item: any;
-}> = ({ width, item }) => (
-  <li
-    className="panel-item-wrapper"
-    key={`carousel-item-${item.id}`}
-    style={
-      {
-        '--items-width': `${width ? width : '500'}px`,
-      } as React.CSSProperties
-    }
-  >
-    <div className="panel-item">
-      <h3 className="panel-item-title">{item.listTitle}</h3>
-      {item.detailText && (
-        <div dangerouslySetInnerHTML={{ __html: item.detailText }} />
+}> = ({ width, item }) => {
+  const device = useMediaQuery();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <li
+      className="panel-item-wrapper"
+      key={`carousel-item-${item.id}`}
+      style={
+        {
+          '--items-width': `${width ? width : '500'}px`,
+        } as React.CSSProperties
+      }
+    >
+      {device === 'mobile' && (
+        <div
+          className="panel-item"
+          onClick={device === 'mobile' ? toggleDropdown : undefined}
+          style={{ cursor: device === 'mobile' ? 'pointer' : 'default' }}
+        >
+          <div className="panel-dropdown">
+            <h3 className="panel-item-title">{item.listTitle}</h3>
+            {AngleDown}
+          </div>
+          {isOpen && (
+            <>
+              {' '}
+              {item.detailText && (
+                <div dangerouslySetInnerHTML={{ __html: item.detailText }} />
+              )}
+              {item.detailLink?.slug && (
+                <div className="card-button">
+                  <Link href={`/${item.detailLink.slug}`}>
+                    <a className="button is-primary mt-4">
+                      {item.detailLinkLabel || 'Learn More'}
+                    </a>
+                  </Link>
+                </div>
+              )}
+            </>
+          )}
+        </div>
       )}
-    </div>
-  </li>
-);
+
+      {device !== 'mobile' && (
+        <div className="panel-item">
+          <h3 className="panel-item-title">{item.listTitle}</h3>
+          {item.detailText && (
+            <div dangerouslySetInnerHTML={{ __html: item.detailText }} />
+          )}
+          {item.detailLink?.slug && (
+            <div className="card-button">
+              <Link href={`/${item.detailLink.slug}`}>
+                <a className="button is-primary mt-4">
+                  {item.detailLinkLabel || 'Learn More'}
+                </a>
+              </Link>
+            </div>
+          )}
+        </div>
+      )}
+    </li>
+  );
+};
