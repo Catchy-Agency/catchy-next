6dcf7304aa11736988569db767848ea9f0b4b664
Francisco Crema <francisco.crema@miracledevs.com>
2024-09-30 10:21:44 -0300

    CWU-32 -> fix the component with cms data

--- components/blocks/LeadCaseStudy.tsx
+++ components/blocks/LeadCaseStudy.tsx
@@ -1,41 +1,42 @@
 import Link from 'next/link';
 import { FC } from 'react';
 import { Image, ResponsiveImageType } from 'react-datocms';
-import { PrimaryPageBySlug_primaryPage_blocks_BannerRecord } from '../../gql/types/PrimaryPageBySlug';
+import { ServicePagesBySlug_service_blocks_CaseStudyTileRecord } from '../../gql/types/ServicePagesBySlug';
 import { IconTopLeadCaseStudy } from '../icons';
 
 export const LeadCaseStudy: FC<{
-  block: PrimaryPageBySlug_primaryPage_blocks_BannerRecord;
+  block: ServicePagesBySlug_service_blocks_CaseStudyTileRecord;
 }> = ({ block }) => {
   return (
     <div className="textStudy-content">
       <div className="study-content">
         <div className="texts-study">
-          {block.titleOverline && (
-            <h3 className="title is-3">{block.titleOverline}</h3>
-          )}
+          {block.overline && <h3 className="title is-3">{block.overline}</h3>}
 
-          {block.title && <h2 className="title is-2">{block.title}</h2>}
+          {block.title && <h1 className="title is-1">{block.title}</h1>}
 
-          {block.text && (
+          {block.caseStudy?.description && (
             <div
               className="textStudy-description"
-              dangerouslySetInnerHTML={{ __html: block.text }}
+              dangerouslySetInnerHTML={{ __html: block.caseStudy?.description }}
             />
           )}
         </div>
 
-        {block.showContactButton === true && (
-          <Link href={'#'}>
-            <a className="button is-primary">{block.contactButtonLabel}</a>
+        {block.caseStudy && (
+          <Link href={block.caseStudy?.slug || ''}>
+            <a className="button is-primary">{block.buttonLabel}</a>
           </Link>
         )}
       </div>
 
       <div className="imgStudy-container">
-        {block.backgroundImage && (
+        {block.caseStudy?.previewImage && (
           <Image
-            data={block.backgroundImage.responsiveImage as ResponsiveImageType}
+            data={
+              block.caseStudy.previewImage
+                .responsiveImage as ResponsiveImageType
+            }
             lazyLoad={false}
             className="imageStudy"
           />
