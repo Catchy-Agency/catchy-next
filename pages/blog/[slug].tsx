fdb15aef550bb16b17dc7d517082a0ddbff157ec
Guido Viani <guido.viani@miracledevs.com>
2024-09-30 12:38:21 -0300

    CWU-15 -> Update styles

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,32 +1,32 @@
-import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
-import { FC } from 'react'
-import { renderMetaTags, useQuerySubscription } from 'react-datocms'
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import { FC } from 'react';
+import { renderMetaTags, useQuerySubscription } from 'react-datocms';
 
-import { BlockSections } from '../../components/BlockSections'
-import { PageError } from '../../components/cms/PageError'
-import { PreviewBanner } from '../../components/cms/PreviewBanner'
-import { Footer } from '../../components/Footer'
-import { Header } from '../../components/Header'
-import { blogPostBySlug } from '../../gql/queries/blog-posts'
+import { BlockSections } from '../../components/BlockSections';
+import { PageError } from '../../components/cms/PageError';
+import { PreviewBanner } from '../../components/cms/PreviewBanner';
+import { Footer } from '../../components/Footer';
+import { Header } from '../../components/Header';
+import { blogPostBySlug } from '../../gql/queries/blog-posts';
 import {
   BlogPostBySlug,
   BlogPostBySlug_blogPost_author,
-} from '../../gql/types/BlogPostBySlug'
+} from '../../gql/types/BlogPostBySlug';
 import {
   createSubscription,
   getBlogPostPaths,
   Subscription,
-} from '../../util/dato-cms'
+} from '../../util/dato-cms';
 
 interface PageProps {
-  subscription: Subscription<BlogPostBySlug>
+  subscription: Subscription<BlogPostBySlug>;
 }
 
 const BlogPostPage: NextPage<PageProps> = ({ subscription }) => {
   const { data, error, status } =
-    useQuerySubscription<BlogPostBySlug>(subscription)
+    useQuerySubscription<BlogPostBySlug>(subscription);
 
   return (
     <>
@@ -40,8 +40,8 @@ const BlogPostPage: NextPage<PageProps> = ({ subscription }) => {
         <PreviewBanner status={status} />
         {error && <PageError error={error} />}
         {data?.header && <Header header={data?.header} />}
-        <header className="section">
-          <div className="container is-max-desktop">
+        <header className="section Blog">
+          <div className="container is-max-widescreen">
             <nav className="breadcrumb" aria-label="breadcrumbs">
               <ul>
                 <li>
@@ -59,7 +59,7 @@ const BlogPostPage: NextPage<PageProps> = ({ subscription }) => {
                 </li>
               </ul>
             </nav>
-            <h1 className="title is-1">{data?.blogPost?.title}</h1>
+            <h1 className="title is-1 title-blog">{data?.blogPost?.title}</h1>
             {data?.blogPost?.author && <ByLine author={data.blogPost.author} />}
             <div className="tags are-medium">
               {data?.blogPost?.categories.map((cat) => (
@@ -72,44 +72,47 @@ const BlogPostPage: NextPage<PageProps> = ({ subscription }) => {
         </header>
         <BlockSections
           blocks={data?.blogPost?.blocks || []}
-          containerMax="desktop"
+          containerMax="widescreen"
         />
       </div>
       {data?.footer && <Footer footer={data?.footer} />}
     </>
-  )
-}
+  );
+};
 
 const ByLine: FC<{ author: BlogPostBySlug_blogPost_author }> = ({ author }) => (
-  <div className="media is-align-items-center mb-5">
+  <div className="media is-align-items-center mb-5 author-blog">
     {author.image?.responsiveImage?.src && (
       <div className="media-left">
         <figure className="image is-64x64">
-          <img className="is-rounded" src={author.image.responsiveImage.src} />
+          <img
+            className="author-image"
+            src={author.image.responsiveImage.src}
+          />
         </figure>
       </div>
     )}
     <div className="media-content">
-      <p className="title is-6">{author.name}</p>
+      <p className="title is-6 author-name">{author.name}</p>
       <p className="subtitle is-6">{author.title}</p>
     </div>
   </div>
-)
+);
 
 export const getStaticPaths: GetStaticPaths = async () => ({
   paths: await getBlogPostPaths(),
   fallback: 'blocking',
-})
+});
 
 export const getStaticProps: GetStaticProps<PageProps> = async (context) => {
   const subscription = await createSubscription<BlogPostBySlug>(
     context,
     blogPostBySlug,
     { slug: context?.params?.slug },
-  )
+  );
   return subscription.initialData?.blogPost
     ? { props: { subscription } }
-    : { notFound: true }
-}
+    : { notFound: true };
+};
 
-export default BlogPostPage
+export default BlogPostPage;
