0a2eda7b48c3c19e4c2c078a65236ce010e16461
Guido Viani <guido.viani@miracledevs.com>
2024-10-14 15:44:19 -0300

    many fix styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,7 +25,7 @@ const App: AppComponent = ({ Component, pageProps }) => {
       <Head>
         {/* Mobile size & bar colors */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="viewport" content="width=device-width, initial-scale=0.9" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#111" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
