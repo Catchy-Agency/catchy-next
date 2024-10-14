2045e8c60164e05b45775722e725e194dd06c621
Francisco Crema <francisco.crema@miracledevs.com>
2024-10-14 12:30:13 -0300

    CWU-187 -> fix styles and accommodating box structure

--- components/blocks/LeadCaseStudy.tsx
+++ components/blocks/LeadCaseStudy.tsx
@@ -20,37 +20,39 @@ export const LeadCaseStudy: FC<{
 
   const image = getImage();
   return (
-    <div className="textStudy-content">
-      <div className="study-content">
-        {block.overline && <h3 className="title is-3">{block.overline}</h3>}
+    <div className="study-wrapper">
+      <div className="textStudy-content">
+        <div className="study-content">
+          {block.overline && <h3 className="title is-3">{block.overline}</h3>}
 
-        {block.title && <h1 className="title is-1">{block.title}</h1>}
+          {block.title && <h1 className="title is-1">{block.title}</h1>}
 
-        {block.caseStudy?.description && (
-          <div
-            className="textStudy-description"
-            dangerouslySetInnerHTML={{ __html: block.caseStudy?.description }}
-          />
-        )}
+          {block.caseStudy?.description && (
+            <div
+              className="textStudy-description"
+              dangerouslySetInnerHTML={{ __html: block.caseStudy?.description }}
+            />
+          )}
 
-        {block.caseStudy && (
-          <Link href={`/work/${block.caseStudy?.slug ?? ''}` || ''}>
-            <a className="button is-primary">{block.buttonLabel}</a>
-          </Link>
-        )}
-      </div>
+          {block.caseStudy && (
+            <Link href={`/work/${block.caseStudy?.slug ?? ''}` || ''}>
+              <a className="button is-primary">{block.buttonLabel}</a>
+            </Link>
+          )}
+        </div>
 
-      <div className="imgStudy-container">
-        {image && (
-          <Image
-            data={image as ResponsiveImageType}
-            lazyLoad={false}
-            className="imageStudy"
-            objectFit="cover"
-          />
-        )}
+        <div className="imgStudy-container">
+          {image && (
+            <Image
+              data={image as ResponsiveImageType}
+              lazyLoad={false}
+              className="imageStudy"
+              objectFit="cover"
+            />
+          )}
 
-        <div className="svg-leadCaseStudy">{IconTopLeadCaseStudy}</div>
+          <div className="svg-leadCaseStudy">{IconTopLeadCaseStudy}</div>
+        </div>
       </div>
     </div>
   );
