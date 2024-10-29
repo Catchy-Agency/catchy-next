4eff2bc5456af170dec41089d3e4142e97988842
Guido Viani <guido.viani@miracledevs.com>
2024-10-29 16:14:04 -0300

    fix error deploy in teams

--- components/blocks/Team.tsx
+++ components/blocks/Team.tsx
@@ -55,7 +55,9 @@ export const Team: FC<{
                   <div className="team-line">
                     {member.name &&
                       (member.link !== null ? (
-                        <Link href={`insights/${member.link?.slug}` || ''}>
+                        <Link
+                          href={`insights/${member.link?.slug || ''}` || ''}
+                        >
                           <a className="title is-size-6 has-text-weight-bold hover-underline">
                             {member.name}
                           </a>
@@ -100,7 +102,7 @@ export const Team: FC<{
                 <div className="team-line">
                   {member.name &&
                     (member.link !== null ? (
-                      <Link href={`insights/${member.link?.slug}` || ''}>
+                      <Link href={`insights/${member.link?.slug || ''} ` || ''}>
                         <a className="title is-size-6 has-text-weight-bold hover-underline">
                           {member.name}
                         </a>
